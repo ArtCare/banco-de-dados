a0742a833bd17c63cd7967ee395e1ec3affc53b4
nathannolacio <nathannolacio04@gmail.com>
2024-04-24 23:19:14 -0300

    Inserindo dados nas tabelas do banco

--- banco_definitivo.sql
+++ banco_definitivo.sql
@@ -47,3 +47,46 @@ create table supervisor (
     constraint pkCompostaSupervisor primary key (idSupervisor, fkCliente),
     constraint fkClienteSupervisor foreign key (fkCliente) references cliente (idCliente)
 );
+
+create table setor (
+	idSetor int auto_increment,
+    fkSensorSetor int,
+    fkSupervisorSetor int,
+    nome varchar(45) not null,
+    andar int not null,
+    constraint pkCompostaSetor primary key (idSetor, fkSensorSetor),
+    constraint fkSensorDoSetor foreign key (fkSensorSetor) references sensor (idSensor),
+    constraint fkSupervisorDoSetor foreign key (fkSupervisorSetor) references supervisor (idSupervisor)
+);
+
+create table registro (
+	idRegistro int auto_increment,
+    fkSensor int,
+    umidade decimal(5,2),
+    temperatura decimal(5,2),
+    dtRegistro timestamp not null default current_timestamp,
+    constraint pkCompostaRegistro primary key (idRegistro, fkSensor),
+    constraint fkSensorRegistro foreign key (fkSensor) references sensor (idSensor)
+);
+
+
+insert into verificacao (tempMax, tempMin, umiMax, umiMin) values
+(20.00, 18.00, 45.00, 40.00);
+
+insert into endereco (cep, numEnd, complemento) values
+('08140-060', '979', 'próximo à avenida paulista');
+
+insert into cliente (fkEndereco, nome, cnpj, rm, email, senha) values
+(1, 'masp', '12345678909876', 'M4$P', 'maspmuseu@outlook.com.br', 'senhamaspmuseu');
+
+insert into sensor (fkVerificacao, nome, tipo) values
+(1, 'dht11', 'temperatura, umidade');
+
+insert into supervisor (fkCliente, nome, permissao) values
+(1, 'bruno', 'sim');
+
+insert into setor (fkSensorSetor, fkSupervisorSetor, nome, andar) values
+(1, 1, 'galeria de arte', 12);
+
+insert into registro (fkSensor, umidade, temperatura) values
+(1, 20.00, 42.00);
